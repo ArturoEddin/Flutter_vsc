6fe571f8a7e827032c96538ba15820418845fefb
kerberus <arturoeddin@gmail.com>
2023-04-25 16:51:22 -0500

    25/04/2023

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,38 +6,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
   int currentPage = 0;
+  final PageController pageController = new PageController();
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-          title: Text("Garra Crema : $currentPage" ),
+          title: Text("Garra Crema : " ),
           elevation: 0,
           backgroundColor: Color.fromARGB(255, 187, 50, 41)),
+
           
-      body: PageView(children: [
-        CustomScreen(color: Color.fromARGB(255, 229, 196, 152)),
-        CustomScreen(color: Color.fromARGB(255, 187, 50, 41))
-      ]),
+     //
+     //
 
       
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentPage,
         onTap: (index) {
-         currentPage =index; 
+         currentPage =index;
+        
          setState(() {}); 
+
+         pageController.animateToPage(index, duration: Duration(seconds: 5), curve: Curves.bounceOut);
+           
         },
         backgroundColor: Colors.black,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white.withOpacity(0.5),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.verified_user_outlined,),  label: "User"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting")
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+          BottomNavigationBarItem(icon: Icon(Icons.car_crash), label: "car")
         ],
       ),
-      
+
+        body: PageView(
+               controller:PageController() ,
+               physics: NeverScrollableScrollPhysics(),
+               children: [
+                CustomScreen(color: Color.fromARGB(255, 229, 196, 152)),
+                CustomScreen(color: Color.fromARGB(255, 187, 50, 41)),
+                CustomScreen(color: Colors.white),
+      ]),
+
     );
   }
 }
